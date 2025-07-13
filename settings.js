6daa2351c8fbb70e50ab51408a9b0e8306dd9141
ryan144212 <mrsingleryan@gmail.com>
2025-07-13 19:10:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,12 +12,12 @@ function convertToBool(text, trueValue = 'true') {
 }
 
 module.exports = {
-  // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID (keep secure)
+  // ===== BOT CORE SETTINGS ====
+  SESSION_ID: process.env.SESSION_ID || "", //malvin~8mlFnYoK#thekAJ-n3E7sNjvMnlLyAnez4AdXIObenf4g2ziGLpQ Your bot's session ID (keep secure)
   PREFIX: getConfig("PREFIX") || ".", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴍᴀʟᴠɪɴ-xᴅ", // Bot display name
-  MODE: getConfig("MODE") || process.env.MODE || "private", // Bot mode: public/private/group/inbox
+  MODE: getConfig("MODE") || process.env.MODE || "private", / Bot mode: public/private/group/inbox
   REPO: process.env.REPO || "https://github.com/XdKing2/MALVIN-XD", // Bot GitHub repo dont change this❗️
   BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys", // Baileys version
 
